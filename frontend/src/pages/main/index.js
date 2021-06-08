8d83c5748fbb6517d5b2d53fe8c944df92f8b4c6
Hogyeong Kim <hogilkim@gmail.com>
2021-06-08 21:36:58 +0900

    finish major functions

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { FormGroup, Label, Input } from 'reactstrap';
+import React, {useState} from 'react';
+import { FormGroup, Label, Input, Form, Button } from 'reactstrap';
 import api from '../../services/api'
 
 export default function Main(){
+    const [file, setFile]=useState(null);
+
+
+    const submitHandler = async(evt)=>{
+        evt.preventDefault();
+
+        const fileData = new FormData();
+
+        fileData.append("file", file);
+
+        try {
+            await api.post("/upload", fileData);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return(
         <div>
-            <FormGroup>
-                <Label for="exampleFile">File</Label>
-                <Input type="file" name="file" id="exampleFile" />
-            </FormGroup>
+            <Form onSubmit={submitHandler}>
+                <FormGroup>
+                    <Label for="exampleFile">File</Label>
+                    <Input type="file" onChange = {evt=>setFile(evt.target.files[0])}/>
+                </FormGroup>
+                <FormGroup>
+                    <Button className="submit-btn">Submit</Button>
+                </FormGroup>
+            </Form>
         </div>
 
     )
